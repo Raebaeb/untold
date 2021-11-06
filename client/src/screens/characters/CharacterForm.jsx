6385367405243940c18480fa1a0fb47f24339fcc
rachelperez <rlb2rlp@gmail.com>
2021-11-06 11:31:17 -0700

    add Form and character state to CharacterForm

--- client/src/screens/characters/CharacterForm.jsx
+++ client/src/screens/characters/CharacterForm.jsx
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { Form } from "../../components";
 import { createCharacter, editCharacter, getCharacter } from "../../services";
-
-import React from 'react';
+import { charFields } from "../../utils/constants";
 
 const CharacterForm = () => {
+  const [character, setCharacter] = useState({
+    name: "",
+    age: "",
+    occupation: "",
+    abilities: "",
+    appearance: "",
+    goal: ""
+  })
+
+
   return (
-    <div>
-      
-    </div>
+    <section>
+      {params.idea ? <h2>Edit Character</h2> : <h2>New Character</h2>}
+      <Form 
+        handleSubmit={handleSubmit}
+        name="Character"
+        fieldsList={charFields}
+        update={setCharacter}
+      />
+    </section>
   );
 };
 
