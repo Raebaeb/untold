a95c83a075d5039482109a7dcd775333422ea95d
rachelperez <rlb2rlp@gmail.com>
2021-11-08 10:49:02 -0800

    conditially render dropdown

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,7 +6,11 @@ import { useEffect } from "react";
 const Nav = (props) => {
   const history = useHistory();
   const { story } = props;
-  const storyElems = [{id: story.id, title: "Characters"}, {id: story.id, title: "Ideas"}, {id: story.id, title: "Scenes"}];
+  const storyElems = [
+    { id: story.id, title: "Characters" },
+    { id: story.id, title: "Ideas" },
+    { id: story.id, title: "Scenes" },
+  ];
 
   const params = useParams();
 
@@ -19,16 +23,26 @@ const Nav = (props) => {
   const handleLogout = async () => {
     await logout();
     props.setUser(null);
-    history.push('/')
+    history.push("/");
   };
   return (
     <nav>
-      <Link to="/home" id="logo">Untold.</Link>
+      <Link to="/home" id="logo">
+        Untold.
+      </Link>
       {props.user ? (
         <>
-          <h3 id='welcome-message'>Welcome back, {props.user.first_name}!</h3>
-          <DropdownContainer linkArray={props.stories}/>
-          {story.length !== 0 ? <DropdownContainer linkArray={storyElems} story={story}/> : <button onClick={handleLogout} className='logout-btn'>Log Out</button>}
+          <h3 id="welcome-message">Welcome back, {props.user.first_name}!</h3>
+          {!props.stories ? null : (
+            <DropdownContainer linkArray={props.stories} />
+          )}
+          {story.length !== 0 ? (
+            <DropdownContainer linkArray={storyElems} story={story} />
+          ) : (
+            <button onClick={handleLogout} className="logout-btn">
+              Log Out
+            </button>
+          )}
         </>
       ) : null}
     </nav>
